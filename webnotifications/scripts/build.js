32c4287ad72b446373d39d1a6b3e35c9d0b56530
emera <emera@servoy.com>
2021-09-17 15:11:00 +0300

    fix error in reading spec file in servoy for version 1.2.0

--- webnotifications/scripts/build.js
+++ webnotifications/scripts/build.js
@@ -3,7 +3,8 @@ var AdmZip = require('adm-zip');
 // creating archives
 var zip = new AdmZip();
 
-zip.addLocalFolder("./webnotifications/", "/webnotifications/");
+zip.addLocalFolder("./webNotifications/", "/webNotifications/");
+zip.addLocalFolder("./toastr/", "/toastr/");
 zip.addLocalFolder("./META-INF/", "/META-INF/");
 zip.addLocalFolder("./dist/servoy/webnotifications/", "/dist/servoy/webnotifications/");
 
